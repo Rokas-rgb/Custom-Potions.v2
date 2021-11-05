ba984817b714a263f45f11ce1f8010cce57c131c
Rokas-rgb <79764461+Rokas-rgb@users.noreply.github.com>
2021-11-05 15:49:16 +0200

    Pain

--- src/main/java/me/rockorbonk/candor/custompotionsv2/BrewingRecipe.java
+++ src/main/java/me/rockorbonk/candor/custompotionsv2/BrewingRecipe.java
@@ -1,8 +1,11 @@
 package me.rockorbonk.candor.custompotionsv2;
 
 import org.bukkit.Material;
+import org.bukkit.block.BrewingStand;
+import org.bukkit.entity.Item;
 import org.bukkit.inventory.BrewerInventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class BrewingRecipe {
 
@@ -83,4 +86,75 @@ public class BrewingRecipe {
     public void setFuelCharge(int fuelCharge) {this.fuelCharge = fuelCharge;}
 
 
+    public class BrewClock extends BukkitRunnable {
+        private BrewerInventory inventory;
+        private BrewingRecipe recipe;
+        private ItemStack[] before;
+        private BrewingStand stand;
+        private int current;
+        public BrewClock(BrewingRecipe recipe, BrewerInventory inventory, int time) {
+            this.recipe = recipe;
+            this.inventory = inventory;
+            this.stand = inventory.getHolder();
+            this.before = inventory.getContents();
+            this.current = time;
+            runTaskTimer(BrewingRecipe.getInstance(), 0L, 1L);
+        }
+
+        @Override
+        public void run() {
+            if(current = 0) {
+                if(inventory.getIngredient().getAmount() > 1) {
+                    ItemStack is = inventory.getIngredient();
+                    is.setAmount(inventory.getIngredient().getAmount() - 1);
+                    inventory.setIngredient(is);
+                } else {
+                    inventory.setIngredient(new ItemStack(Material.AIR));
+                }
+                ItemStack newFuel = recipe.getFuel();
+                if(recipe.getFuel() != null && recipe.getFuel().getType() != Material.AIR && recipe.getFuel().getAmount() > 0) {
+                    int count = 0;
+                    while (inventory.getFuel().getAmount() > 0 && stand.getFuelLevel() + recipe.fuelCharge < 100) {
+                        stand.setFuelLevel(stand.getFuelLevel() + recipe.fuelSet);
+                        count++;
+                    }
+                    if(inventory.getFuel().getAmount() == 0) {
+                        newFuel = new ItemStack(Material.AIR);
+                    } else{
+                        stand.setFuelLevel(100);
+                        newFuel.setAmount(inventory.getFuel().getAmount() - count);
+                    }
+                } else{
+                    newFuel = new ItemStack(Material.AIR);
+                }
+                inventory.setFuel(newFuel);
+                for (int i = 0; i < 3; i++) {
+                    if(inventory.getItem(i) == null || inventory.getItem(i).getType() == Material.AIR) {
+                        continue;
+                    }
+                    recipe.getAction().brew(inventory, inventory.getItem(i), (Item) ingredient);
+                }
+                stand.setFuelLevel(stand.getFuelLevel() - recipe.fuelCharge);
+                cancel();
+                return;
+            }
+            current--;
+            stand.setBrewingTime(current);
+            stand.update(true);
+        }
+        public boolean searchChanged(ItemStack[] before, ItemStack[] after, boolean mode) {
+            for (int i = 0; i < before.length; i++) {
+                if((before[i] != null && after[i] == null || (before[i] == null && after[i] != null))) {
+                    return false;
+                } else {
+                    if(mode && !before[i].isSimilar(after[i])) {
+                        return false;
+                    } else if(!mode && !(before[i].getType() == after[i].getType())) {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
 }
